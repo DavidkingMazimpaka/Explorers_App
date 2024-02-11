70ad9916861529222743ac9395f4fd9b2e9a9d84
mnshimiyimana <m.nshimiyim@alustudent.com>
2024-02-11 10:21:50 +0200

    Profile updated

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
 }
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +35,7 @@ class ProfilePage extends StatelessWidget {
 }
 
 class NavigationPanel extends StatelessWidget {
-  const NavigationPanel({super.key});
+  const NavigationPanel({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -47,38 +47,38 @@ class NavigationPanel extends StatelessWidget {
           // Top section with the image
           Padding(
             padding: const EdgeInsets.only(
-                top: 20.0,
-                bottom: 30.0,
-                right: 90.0), // Adjust the padding as needed
+              top: 20.0,
+              bottom: 30.0,
+            ), // Adjust the padding as needed
             child: Image.asset(
-              'welcome.png', // Replace with your actual image path
+              'images/explorer.png', // Replace with your actual image path
               width: 100, // Set the width as needed
               height: 100, // Set the height as needed
             ),
           ),
           // Middle section with menu items, no padding needed between items
-          const ListTile(
-            leading: Icon(Icons.place, color: Colors.white),
-            title: Text('Destinations', style: TextStyle(color: Colors.white)),
+          ListTile(
+            leading: const Icon(Icons.place, color: Colors.white),
+            title: const Text('Destinations', style: TextStyle(color: Colors.white)),
           ),
-          const ListTile(
-            leading: Icon(Icons.settings, color: Colors.white),
-            title: Text('Settings', style: TextStyle(color: Colors.white)),
+          ListTile(
+            leading: const Icon(Icons.settings, color: Colors.white),
+            title: const Text('Settings', style: TextStyle(color: Colors.white)),
           ),
-          const ListTile(
-            leading: Icon(Icons.exit_to_app, color: Colors.white),
-            title: Text('Log Out', style: TextStyle(color: Colors.white)),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app, color: Colors.white),
+            title: const Text('Log Out', style: TextStyle(color: Colors.white)),
           ),
           // Spacer to push the bottom content to the end
           const Spacer(),
           // Bottom section with the user's username
-          const Padding(
-            padding: EdgeInsets.all(16.0),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
             child: Row(
               children: [
-                Icon(Icons.account_circle, color: Colors.white, size: 24.0),
-                SizedBox(width: 8.0),
-                Text('User username', style: TextStyle(color: Colors.white)),
+                const Icon(Icons.account_circle, color: Colors.white, size: 24.0),
+                const SizedBox(width: 8.0),
+                const Text('User username', style: TextStyle(color: Colors.white)),
               ],
             ),
           ),
@@ -88,38 +88,27 @@ class NavigationPanel extends StatelessWidget {
   }
 }
 
+
 class MainPanel extends StatelessWidget {
-  const MainPanel({super.key});
+  const MainPanel({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
-        padding: const EdgeInsets.all(16.0), // Adjust padding as needed
+        padding: const EdgeInsets.all(30.0), // Adjust padding as needed
         color: Colors.white, // Set the background color to white
         child: const SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Card(
-                elevation: 4.0,
-                child: ProfileSection(),
-              ),
-              SizedBox(height: 16), // Space between cards
-              Card(
-                elevation: 4.0,
-                child: FavoriteDestinations(),
-              ),
-              SizedBox(height: 16), // Space between cards
-              Card(
-                elevation: 4.0,
-                child: TripsSection(),
-              ),
-              SizedBox(height: 16), // Space between cards
-              Card(
-                elevation: 4.0,
-                child: FeedbackForm(),
-              ),
+              ProfileSection(),
+              SizedBox(height: 35), // Space between sections
+              FavoriteDestinations(),
+              SizedBox(height: 60), // Space between sections
+              TripsSection(),
+              SizedBox(height: 80), // Space between sections
+              FeedbackForm(),
             ],
           ),
         ),
@@ -127,78 +116,95 @@ class MainPanel extends StatelessWidget {
     );
   }
 }
-// This widget is responsible for displaying the user's profile information.
 
 class ProfileSection extends StatelessWidget {
-  const ProfileSection({super.key});
+  const ProfileSection({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0), // Padding inside the card
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          CircleAvatar(
-            // Replace with your user's avatar image
-            backgroundColor: Colors.grey,
-            radius: 40, // Adjust the size as needed
-          ),
-          SizedBox(width: 10), // Add space between the avatar and the username
-          Text(
-            '@Username',
-            style: TextStyle(
-              fontSize: 24, // Adjust the font size as needed
-              fontWeight: FontWeight.bold, // Make the username bold
+    return Center( // Center the ProfileSection vertically
+      child: Padding(
+        padding: const EdgeInsets.all(30.0), // Padding inside the section
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // Align content vertically
+          children: [
+            CircleAvatar(
+              // Replace with your user's avatar image
+              backgroundColor: Colors.grey,
+              radius: 40, // Adjust the size as needed
             ),
-          ),
-        ],
+            const SizedBox(height: 10), // Add space between the avatar and the username
+            Text(
+              '@Username', // Display the username
+              style: TextStyle(
+                fontSize: 24, // Adjust the font size as needed
+                fontWeight: FontWeight.bold, // Make the username bold
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
+
 class FavoriteDestinations extends StatelessWidget {
-  const FavoriteDestinations({super.key});
+  const FavoriteDestinations({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This list can be dynamic based on user's favorite destinations
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 10.0), // Add vertical padding
-          child: Text(
-            'Favorite Destinations',
-            style: TextStyle(
-              fontSize: 20, // Adjust the font size as needed
-              fontWeight: FontWeight.bold, // Make the title bold
+    
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0), //border radius
+      ),
+      color: Colors.grey, //background color
+      child: Container(
+        padding: EdgeInsets.only(top: 10.0, bottom: 20.0), // Add bottom padding
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(7.0),  //padding inside
+              child: Text(
+                'Favorite Destinations',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
-          ),
+
+            ListTile(
+              leading: CircleAvatar(
+                //picture
+                backgroundImage: AssetImage('path/to/your/image.png'),
+                radius: 30,
+              ),
+              title: Text('Destination name'),
+              subtitle: Text('Destination description'),
+            ),
+            // Repeat ListTiles for each destination
+          ],
         ),
-        // You can generate these ListTiles with a ListView.builder in a real app
-        ListTile(
-          leading: Icon(Icons.favorite, color: Colors.red),
-          title: Text('Destination name'),
-          subtitle: Text('Destination description'),
-        ),
-        // Repeat ListTiles for each destination
-      ],
+      ),
     );
   }
 }
 
+
+
+
+
 class TripsSection extends StatelessWidget {
-  const TripsSection({super.key});
+  const TripsSection({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding:
-          const EdgeInsets.symmetric(vertical: 20.0), // Add vertical padding
-      child: const Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center, // Center content horizontally
         children: [
           Text(
             'My Trips',
@@ -207,20 +213,18 @@ class TripsSection extends StatelessWidget {
               fontWeight: FontWeight.bold, // Make the title bold
             ),
           ),
-          Card(
-            child: Padding(
-              padding: EdgeInsets.all(16.0), // Add padding inside the card
-              child: Column(
-                children: [
-                  Icon(
-                    Icons.card_travel,
-                    size: 48, // Adjust the icon size as needed
-                    color: Colors.grey, // Set the icon color
-                  ),
-                  Text('No Trips Yet'),
-                  // More content can be added here
-                ],
-              ),
+          Padding(
+            padding: const EdgeInsets.all(16.0), // Add padding inside the section
+            child: Column(
+              children: [
+                Icon(
+                  Icons.card_travel,
+                  size: 48, // Adjust the icon size as needed
+                  color: Colors.grey, // Set the icon color
+                ),
+                Text('No Trips Yet'),
+                // More content can be added here
+              ],
             ),
           ),
         ],
@@ -230,13 +234,12 @@ class TripsSection extends StatelessWidget {
 }
 
 class FeedbackForm extends StatelessWidget {
-  const FeedbackForm({super.key});
+  const FeedbackForm({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding:
-          const EdgeInsets.symmetric(vertical: 20.0), // Add vertical padding
+      padding: const EdgeInsets.symmetric(vertical: 20.0), // Add vertical padding
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
