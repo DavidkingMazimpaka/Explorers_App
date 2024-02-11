f60870fe8d3f65b37d0ea67cd02f8a79ecd9b483
rumunyana <113432535+rumunyana@users.noreply.github.com>
2024-02-11 20:41:40 +0200

    changes

--- lib/screens/logout_screen.dart
+++ lib/screens/logout_screen.dart
@@ -5,6 +5,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  final Map<String, WidgetBuilder> _routes = {
+    '/logout': (context) => const LogoutScreen(),
+  };
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,7 +18,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LogoutScreen(),
+      home: const LogoutScreen(),
+      routes: _routes,
     );
   }
 }
@@ -21,27 +28,51 @@ class LogoutScreen extends StatelessWidget {
   static const String routeName = '/logout';
 
   const LogoutScreen({Key? key}) : super(key: key);
+
+  void _showLogoutConfirmationDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirm Logout'),
+          content: const Text('Are you sure you want to log out?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(), // Dismiss dialog
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Dismiss dialog
+                Navigator.pushReplacementNamed(
+                    context, '/login'); // Navigate to login
+              },
+              child: const Text('Logout'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Logout'),
+        title: const Text('Logout'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               'Are you sure you want to logout?',
               style: TextStyle(fontSize: 20),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                // Perform logout action here
-                Navigator.pushNamed(context, '/login');
-              },
-              child: Text('Logout'),
+              onPressed: () => _showLogoutConfirmationDialog(context),
+              child: const Text('Confirm Logout'),
             ),
           ],
         ),
